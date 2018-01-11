b942e4dac0caf1e39be3ba21d70eb573a172e5f7
Mark O'Reilly <moreil22@uwo.ca>
2018-01-11 16:51:15 -0500

    Plotting Time Series

--- HCP_Analysis.py
+++ HCP_Analysis.py
@@ -1,24 +1,30 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from nipy import load_image
 from nilearn import input_data
 
-func_img = load_image(filename)
-func_data = func_img.get_data()
+#Load image and extract data
+#func_img = load_image('/home/moreilly/HCP_BIDS/sub-02/func/sub-02_task-rest_bold.nii.gz')
+#func_data = func_img.get_data()
 
-class ROI:
+#Define ROI class
+# class ROI:
+# 	#Define seed voxel
+# 	def __init__(self, x, y, z):
+# 		self.xcoord = x
+# 		self.ycoord = y
+# 		self.zcoord = z
+# 		self.coords = [x,y,z]
 
-	def __init__(self, x, y, z):
-		self.xcoord = x
-		self.ycoord = y
-		self.zcoord = z
-		self.coords = [x,y,z]
+# 	#Create ROI sphere around seed voxel
+# 	def makeSphere(self, rad, TR, mem, mem_lev):
+# 		self.data = func_data[self.xcoord, self.ycoord, self.zcoord,:]
+# 		self.masker = input_data.NiftiSpheresMasker(
+# 			self.coords, rad, TR, mem, mem_lev)
 
-	def makeSphere(self, rad, TR, mem, mem_lev):
-		self.data = func_img[self.xcoord, self.ycoord, self.zcoord,:]
-		self.masker = input_data.NiftiSpheresMasker(
-			self.coords, rad, TR, mem, mem_lev)
-
-PCC = ROI(1,5,8)
-PCC_ROI = PCC.makeSphere(5, 1000, 'PCCseedcache', 1)
+PCC = [(34,45,34)]
+seed_masker = input_data.NiftiSpheresMasker(PCC, radius = 5, t_r = 1, memory='PCCcache', memory_level=1)
+seed_time_series = seed_masker.fit_transform('/home/moreilly/HCP_BIDS/sub-02/func/sub-02_task-rest_bold.nii.gz')
+plt.plot(seed_time_series)
+plt.show()
 
